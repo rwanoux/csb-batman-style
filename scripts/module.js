d5f13874f35748749bbf8c414fe735c4849c94c6
rwanoux <erwan_lemaire@yahoo.fr>
2024-08-28 17:43:07 +0200

    first init

---
+++ scripts/module.js
@@ -0,0 +1,16 @@
+Hooks.once('init', async function () {
+   console.log("_________marvel style__________")
+
+});
+
+
+Hooks.on('renderPause', () => {
+   let pause = document.getElementById("pause");
+   let imgPause = pause.querySelector("img");
+   imgPause.src = "modules/csb-batman-style/assets/img/bgcc_logo.webp"
+   imgPause.style.animation = "none"
+   imgPause.style.top = "-5rem"
+   imgPause.style.opacity = "1"
+
+   pause.querySelector("figcaption").innerHTML = "tout est calme à Gotham"
+})
